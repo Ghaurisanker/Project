b74ec326c8a87f4e8bf3be76f6f51674b9a21017
Ghauri Sanker <ghauri98.gs@outlook.com>
2021-09-05 16:21:28 +0530

    Fourth Commit

---
+++ SleeExample/sbb/rootsbb/src/main/java/org/example/slee/sbb/rootsbb/RootSbb.java
@@ -0,0 +1,49 @@
+package org.example.slee.sbb.rootsbb;
+
+import javax.slee.*;
+
+public class RootSbb implements Sbb {
+    public void setSbbContext(SbbContext sbbContext) {
+
+    }
+
+    public void unsetSbbContext() {
+
+    }
+
+    public void sbbCreate() throws CreateException {
+
+    }
+
+    public void sbbPostCreate() throws CreateException {
+
+    }
+
+    public void sbbActivate() {
+
+    }
+
+    public void sbbPassivate() {
+
+    }
+
+    public void sbbLoad() {
+
+    }
+
+    public void sbbStore() {
+
+    }
+
+    public void sbbRemove() {
+
+    }
+
+    public void sbbExceptionThrown(Exception e, Object o, ActivityContextInterface activityContextInterface) {
+
+    }
+
+    public void sbbRolledBack(RolledBackContext rolledBackContext) {
+
+    }
+}
